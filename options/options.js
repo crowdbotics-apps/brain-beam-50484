4337f10c517cf5dbfa7f8c4fcbfa6fe6dc3553fc
Crowdbotics <team@crowdbotics.com>
2024-12-23 13:44:08 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "brain-beam-50484",
+  url: "https://brain-beam-50484.botics.co",
+  api: "https://brain-beam-50484.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
